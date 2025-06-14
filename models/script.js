630513006fb845f9219252189bfb467cd1e29088
Taro2727 <lautarogameryt27@gmail.com>
2025-06-14 01:02:03 -0300

    empece el js del registro del foro, hice modificaciones en el html para q este sea compatible con el js e/y hice un archivo en python para hacer la conexion con una bd mediane flask

---
+++ models/script.js
@@ -0,0 +1,11 @@
+// esto funca para el registro de la pág
+//guardamo el formulario de registro en regi
+let regi = document.getElementById('registro');
+regi.addEventListener('submit',async function (event){
+    //cuando apretas el boton de registrarte (cndo haces submit) va a pasar esto: (event)
+    event.preventDefault(); 
+    //agarra los datos del formulario (email,Contra)
+    const email = document.getElementById('email').value;
+    const contra = document.getElementById('password').value;
+    const confirmcontra = document.getElementById('confirm').value;
+})
