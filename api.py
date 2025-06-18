7fe153aa4948359496eb4e4623f670c88385d0cd
lauty <lautaritog2239@gmail.com>
2025-06-18 00:58:45 -0300

    cambie el nombre de la variable de la funcion de la ruta /crearcuenta: de inicio a regi

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 #el =Flask(__name__) dice que va a agarrar una funcion de flask
 #y que esa funcion va a usar un nombre (una ruta)
 @app.route('/crearcuenta')
-def inicio():
+def regi():
     return render_template('index/indexcrearcuenta.html')
 #sin una / inicial antes del index/ pq ya stams en templates gracias al render_template
 @app.route('/crearcuenta/registrar',methods=['POST'])
