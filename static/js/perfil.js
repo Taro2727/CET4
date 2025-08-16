b312b48a76b4a0002ceb8e00640f30a372a549b3
lauty <lautaritog2239@gmail.com>
2025-08-16 11:10:52 -0300

    modal funciona (para editar perfil) el problema era que habia codigo repeteido (lo que borre en perfil.js) y habia un error de sintaxis en modalperfil.js)

--- static/js/perfil.js
+++ static/js/perfil.js
@@ -45,26 +45,26 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Obtiene los elementos del DOM
-    const modal = document.getElementById('myModal');
-    const btn = document.getElementById('openModalBtn');
-    const span = document.getElementsByClassName("close-btn")[0];
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Obtiene los elementos del DOM
+//     const modal = document.getElementById('myModal');
+//     const btn = document.getElementById('openModalBtn');
+//     const span = document.getElementsByClassName("close-btn")[0];
 
-    // Cuando el usuario hace clic en el botón, se abre el modal
-    btn.onclick = function() {
-        modal.style.display = "flex"; // Usa "flex" para centrar el modal
-    }
+//     // Cuando el usuario hace clic en el botón, se abre el modal
+//     btn.onclick = function() {
+//         modal.style.display = "flex"; // Usa "flex" para centrar el modal
+//     }
 
-    // Cuando el usuario hace clic en <span> (x), se cierra el modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+//     // Cuando el usuario hace clic en <span> (x), se cierra el modal
+//     span.onclick = function() {
+//         modal.style.display = "none";
+//     }
 
-    // Cuando el usuario hace clic en cualquier lugar fuera del modal, se cierra
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-});
+//     // Cuando el usuario hace clic en cualquier lugar fuera del modal, se cierra
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
+// });
