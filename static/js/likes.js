6ccd91d490c524cad9ea8318d737e5533fd48cf4
Salva-JS <martinrodriguez7411@gmail.com>
2025-08-17 22:33:27 -0300

    le puse estilo a la pagina "ver mis likes"

--- static/js/likes.js
+++ static/js/likes.js
@@ -14,12 +14,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 const div = document.createElement("div");
                 div.classList.add("post-likeado");
                 div.innerHTML = `
-                    <div id="commentsSection" class="comentarios-anteriores">
+                    <div class="comentarios-anteriores">
+                    <div class="contenido-titulo">
                     <h3>${post.titulo}</h3>
+                    </div>
+                    <div class="contenido-texto">
                     <p>${post.cont}</p>
+                    </div>
+
+                    <div class="contenido-info">
                     <p><strong>Autor:</strong> ${post.nom_usu}</p>
                     <p><strong>Materia:</strong> ${post.nom_mat}</p>
                     <p><em>Fecha:</em> ${new Date(post.fecha).toLocaleString()}</p>
+                    </div>
+                    
                     </div>
                 `;
                 container.appendChild(div);
